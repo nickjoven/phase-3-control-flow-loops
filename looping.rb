5cc905b5c0212e46d34bb5c893757dd8bedcc8ba
Nicholas <nicholasjoven@gmail.com>
2022-08-09 14:59:58 -0400

    completed lab

--- looping.rb
+++ looping.rb
@@ -1,5 +1,10 @@
 def happy_new_year
-  # your code here
+  count = 10
+  until count <= 0
+    puts count
+    count -= 1
+  end
+  puts "Happy New Year!"
 end
 
 # No need to modify this code! Use this to implement the fizzbuzz_printer method.
@@ -16,9 +21,18 @@ def fizzbuzz(num)
 end
 
 def fizzbuzz_printer
-  # your code here
+  (1..100).each do | num |
+    puts fizzbuzz(num)
+  end
 end
 
+# Take the first character of the string. Take the second character of the string and prepend it to the first one
+# Take the third character of the string and add prepend it to the second and first. 
 def reverse_string(str)
   # your code here
+  new_str = ""
+  str.length.times do | i |
+    new_str = str[i] + new_str
+  end
+  new_str
 end
